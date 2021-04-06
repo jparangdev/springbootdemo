7ec7aff6025c7301868d446ffe261f11e633c1a3
jungho <jungho@juvis.co.kr>
2021-04-06 11:46:25 +0900

    init

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+## Spring Boot dev
+
+
++ 초기 추가 패키지  
+  spring web  
+  lombok
+  
+
---
+++ src/test/java/kr/co/jparangdev/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jparangdev.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
