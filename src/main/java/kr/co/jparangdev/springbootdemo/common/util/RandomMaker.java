b4af9055c4618119b9a0dd48e7e4ec71b3413025
jungho <jungho@juvis.co.kr>
2021-04-08 18:11:59 +0900

    init -2

---
+++ src/main/java/kr/co/jparangdev/springbootdemo/common/util/RandomMaker.java
@@ -0,0 +1,27 @@
+package kr.co.jparangdev.springbootdemo.common.util;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/* 랜덤 숫자 유틸 클래스*/
+public class RandomMaker {
+
+    Random random;
+
+    public RandomMaker(Random random) {
+        this.random = new Random();
+    }
+
+    public int getRandomInt(int start, int end) {
+        random.setSeed(System.currentTimeMillis());
+        return random.nextInt(start+end)+start;
+    }
+
+    public List<Integer> getRandomIntegerList(int start, int end, int size) {
+        List<Integer> list = new LinkedList<>();
+        random.setSeed(System.currentTimeMillis());
+        for(int i =0; i<size; i++) list.add(random.nextInt(start+end)+start);
+        return list;
+    }
+}
