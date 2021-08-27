b0ead9242e4372e07d086a74b53914736ffc697d
jparangdev <jparangdev@gmail.com>
2021-08-27 23:30:51 +0900

    junit5에선 Extendwith을 쓴다.

--- src/test/java/kr/co/jparangdev/springbootdemo/biz/user/service/UserServiceTest.java
+++ src/test/java/kr/co/jparangdev/springbootdemo/biz/user/service/UserServiceTest.java
@@ -6,13 +6,19 @@ import kr.co.jparangdev.springbootdemo.common.annotation.TimeCheck;
 import kr.co.jparangdev.springbootdemo.common.constants.TimeEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class UserServiceTest {
+
+
 
     @Autowired
     UserRepository userRepository;
