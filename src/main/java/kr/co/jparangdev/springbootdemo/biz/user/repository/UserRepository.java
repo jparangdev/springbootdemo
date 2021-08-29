9a37665ae941973028db3051f64eac16937f6f72
jparangdev <jparangdev@gmail.com>
2021-08-29 22:45:28 +0900

    junit5에선 Extendwith을 쓴다.

--- src/main/java/kr/co/jparangdev/springbootdemo/biz/user/repository/UserRepository.java
+++ src/main/java/kr/co/jparangdev/springbootdemo/biz/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package kr.co.jparangdev.springbootdemo.biz.user.repository;
 import kr.co.jparangdev.springbootdemo.biz.user.vo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/test/java/kr/co/jparangdev/springbootdemo/biz/user/service/UserServiceTest.java
+++ src/test/java/kr/co/jparangdev/springbootdemo/biz/user/service/UserServiceTest.java
@@ -2,23 +2,20 @@ package kr.co.jparangdev.springbootdemo.biz.user.service;
 
 import kr.co.jparangdev.springbootdemo.biz.user.repository.UserRepository;
 import kr.co.jparangdev.springbootdemo.biz.user.vo.User;
-import kr.co.jparangdev.springbootdemo.common.annotation.TimeCheck;
-import kr.co.jparangdev.springbootdemo.common.constants.TimeEnum;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UserServiceTest {
-
-
 
     @Autowired
     UserRepository userRepository;
@@ -28,6 +25,14 @@ class UserServiceTest {
         User user = new User("jparangdev","jpd@email.com","JB099");
         userRepository.save(user);
         userRepository.findAll().forEach(System.out::println);
+        long result = userRepository.count();
+        assertEquals(5l,result);
+    }
+
+    @Test
+    void findByEmailTestNoMember() {
+        Optional<User> user = userRepository.findByEmail("jpd@email.com");
+        assertTrue(user.isEmpty());
     }
 
 
