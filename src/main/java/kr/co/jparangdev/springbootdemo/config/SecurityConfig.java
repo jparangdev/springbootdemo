4d25c72c7f6ba533b1f28d1c3e45824c37aaa74e
jparangdev <jparangdev@gmail.com>
2021-08-30 19:00:08 +0900

    시큐리티 설정 추가

---
+++ src/main/java/kr/co/jparangdev/springbootdemo/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package kr.co.jparangdev.springbootdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
