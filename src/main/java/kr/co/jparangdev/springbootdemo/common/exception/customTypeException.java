92eebe07a7f12fbca150b5db85ab005a47bb807d
jparangdev <jparangdev@gmail.com>
2021-09-23 19:02:23 +0900

    익셉션 설정 공부하자

---
+++ src/main/java/kr/co/jparangdev/springbootdemo/common/exception/customTypeException.java
@@ -0,0 +1,5 @@
+package kr.co.jparangdev.springbootdemo.common.exception;
+
+public class customTypeException extends RuntimeException{
+
+}
---
+++ src/main/java/kr/co/jparangdev/springbootdemo/common/exceptionHandler/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package kr.co.jparangdev.springbootdemo.common.exceptionHandler;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+}
